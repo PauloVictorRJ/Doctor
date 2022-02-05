eb342a58fa122dfdd8ffd545752c250cb260ae73
Paulo Victor <c4.paulovictor@gmail.com>
2022-02-04 21:40:04 -0800

    Denis Debugger

--- app/src/main/java/com/example/doctor/MainActivity.kt
+++ app/src/main/java/com/example/doctor/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.doctor
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
